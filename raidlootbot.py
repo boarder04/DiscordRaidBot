078dafdaf2a31ef34fc912f4dbb92e88e99e8915
Stephen <45674438+StephenLockard@users.noreply.github.com>
2023-12-24 08:17:01 -0500

    Update raidlootbot.py

--- raidlootbot.py
+++ raidlootbot.py
@@ -1,128 +1,79 @@
 import asyncio
 import random
 import discord
-from  discord import app_commands, Interaction
-from  discord.app_commands import Range
+from discord import app_commands, Interaction
+from discord.app_commands import Range
 import config
-
 
 class RandomBidView(discord.ui.View):
     def __init__(self, timeout: int):
-        self.users: list[str] = []
+        self.user_bids = {}  # Store user bids as a dict
         super().__init__(timeout=timeout)
 
-    @discord.ui.button(label='Main', custom_id='raidMain_bid_join')
-    async def join(self, interaction: Interaction, button: discord.ui.Button):
-        """Joins the bid."""
+    async def handle_bid(self, interaction: Interaction, bid_type: str):
+        user_id = interaction.user.id  # Use user ID to uniquely identify users
 
-        if (interaction.user.display_name + " (RAID MAIN)") in self.users:
-            return await interaction.response.send_message(
+        if user_id in self.user_bids:
+            await interaction.response.send_message(
                 embed=discord.Embed(
                     color=discord.Color.red(),
-                    description='You already joined this bid!'
+                    description=f'You have already placed a bid as **{self.user_bids[user_id]}**!'
+                ),
+                ephemeral=True
+            )
+        else:
+            self.user_bids[user_id] = bid_type
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    color=discord.Color.green(),
+                    description=f'You have successfully bid as **{bid_type}**.'
                 ),
                 ephemeral=True
             )
 
-        self.users.append(interaction.user.display_name + " (RAID MAIN)")
-
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                color=discord.Color.green(),
-                description='Successfully joined the bid!'
-            ),
-            ephemeral=True
-        )
+    @discord.ui.button(label='Main', custom_id='raidMain_bid_join')
+    async def join_main(self, interaction: Interaction, button: discord.ui.Button):
+        await self.handle_bid(interaction, "RAID MAIN")
 
     @discord.ui.button(label='Approved Box', custom_id='raidBox_bid_join')
-    async def joinBox(self, interaction: Interaction, button: discord.ui.Button):
-        """Joins the bid for an approved raid box."""
+    async def join_box(self, interaction: Interaction, button: discord.ui.Button):
+        await self.handle_bid(interaction, "RAID BOX")
 
-        if (interaction.user.display_name + " (RAID BOX)") in self.users:
-            return await interaction.response.send_message(
-                embed=discord.Embed(
-                    color=discord.Color.red(),
-                    description='You already joined this bid!'
-                ),
-                ephemeral=True
-            )
-
-        self.users.append(interaction.user.display_name + " (RAID BOX)")
-
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                color=discord.Color.green(),
-                description='Successfully joined the bid!'
-            ),
-            ephemeral=True
-        )
-    
     @discord.ui.button(label='Alt', custom_id='alt_bid_join')
-    async def joinAlt(self, interaction: Interaction, button: discord.ui.Button):
-        """Joins the bid for an approved raid box."""
-
-        if (interaction.user.display_name + " (ALT)") in self.users:
-            return await interaction.response.send_message(
-                embed=discord.Embed(
-                    color=discord.Color.red(),
-                    description='You already joined this bid!'
-                ),
-                ephemeral=True
-            )
-
-        self.users.append(interaction.user.display_name + " (ALT)")
-
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                color=discord.Color.green(),
-                description='Successfully joined the bid!'
-            ),
-            ephemeral=True
-        )
+    async def join_alt(self, interaction: Interaction, button: discord.ui.Button):
+        await self.handle_bid(interaction, "ALT")
 
     @discord.ui.button(label='Leave', custom_id='bid_leave')
     async def leave(self, interaction: Interaction, button: discord.ui.Button):
-        """Leaves the bid."""
+        user_id = interaction.user.id
 
-        if (
-            (interaction.user.display_name + " (RAID MAIN)") not in self.users
-        and (interaction.user.display_name + " (RAID BOX)") not in self.users
-        and (interaction.user.display_name + " (ALT)") not in self.users
-        ):
-            return await interaction.response.send_message(
+        if user_id not in self.user_bids:
+            await interaction.response.send_message(
                 embed=discord.Embed(
-                    color=discord.Color.red(),
-                    description='You did not join this bid!'
+                    color=discord.Color.red(), 
+                    description='You have not joined the bid!'
+                ),
+                ephemeral=True
+            )
+        else:
+            bid_type = self.user_bids.pop(user_id)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    color=discord.Color.green(), 
+                    description=f'You have left the bid as **{bid_type}**.'
                 ),
                 ephemeral=True
             )
 
-        if (interaction.user.display_name + " (RAID MAIN)") in self.users:
-            self.users.remove(interaction.user.display_name + " (RAID MAIN)")
-
-        if (interaction.user.display_name + " (RAID BOX)") in self.users:
-            self.users.remove(interaction.user.display_name + " (RAID BOX)")
-        
-        if (interaction.user.display_name + " (ALT)") in self.users:
-            self.users.remove(interaction.user.display_name + " (ALT)")
-
-        await interaction.response.send_message(
-            embed=discord.Embed(
-                color=discord.Color.green(),
-                description='Successfully left the bid!'
-            ),
-            ephemeral=True
-        )
 
 class PlatBidView(discord.ui.View):
-    def __init__(self, timeout: int, min_bid: int):
-        self.bids = {}
-        self.min_bid = min_bid
+    def __init__(self, timeout: int):
         super().__init__(timeout=timeout)
+        self.bids = {}
 
     @discord.ui.button(label='Bid', style=discord.ButtonStyle.green, custom_id='bid_button')
     async def bid_button(self, interaction: Interaction, button: discord.ui.Button):
-        modal = PlatBidModal(self)
+        modal = PlatBidModal(self.bids)
         await interaction.response.send_modal(modal)
 
     @discord.ui.button(label='Leave Bid', style=discord.ButtonStyle.red, custom_id='leave_bid_button')
@@ -137,10 +88,9 @@ class PlatBidView(discord.ui.View):
         return sorted(self.bids.items(), key=lambda x: x[1], reverse=True)
 
 class PlatBidModal(discord.ui.Modal):
-    def __init__(self, view):
-        title = f"Place Your Bid (Minimum Bid: {view.min_bid:,})"
-        super().__init__(title=title)
-        self.view = view
+    def __init__(self, bids):
+        super().__init__(title=f"Place Your Bid (Minimum Bid: {CustomBot.min_bid:,})")
+        self.bids = bids
 
     bid_amount = discord.ui.TextInput(label="Bid Amount", placeholder="Enter your bid amount here")
 
@@ -152,42 +102,39 @@ class PlatBidModal(discord.ui.Modal):
             await interaction.response.send_message("Please enter a valid number for the bid amount.", ephemeral=True)
             return
 
-        if bid_amount < self.view.min_bid:
-            await interaction.response.send_message(f"Your bid must be at least {self.view.min_bid}.", ephemeral=True)
+        if bid_amount < CustomBot.min_bid:
+            await interaction.response.send_message(f"Your bid must be at least {CustomBot.min_bid}.", ephemeral=True)
             return
 
-        self.view.bids[interaction.user.display_name] = bid_amount
+
+        self.bids[interaction.user.display_name] = bid_amount
         await interaction.response.send_message(f"Your bid of {bid_amount} has been placed.", ephemeral=True)
 
 class CustomBot(discord.Client):
-    tree: app_commands
+    min_bid = 1  # Class attribute for minimum bid
 
     def __init__(self):
         intents = discord.Intents.default()
+        intents.members = True
         super().__init__(intents=intents, help_command=None)
         self.tree = app_commands.CommandTree(self)
-        self.min_bid = 1  # Default minimum bid
 
     async def setup_hook(self):
-        # Syncs the slash commands
+        self.tree.clear_commands(guild=None)
+        self.tree.add_command(start_random)
+        self.tree.add_command(start_bids)
+        self.tree.add_command(set_min_bid)
         await self.tree.sync()
 
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
 
 bot = CustomBot()
 
-
-@bot.event
-async def on_ready():
-    print(f'-------------------- Bot is ready! --------------------')
-    print(f'Logged in as {bot.user}'.center(55))
-    print(f'-------------------------------------------------------')
-
-
 @bot.tree.command(name='random')
 @app_commands.guild_only()
-@app_commands.default_permissions(administrator=True)
 @app_commands.describe(item='The name of the item', classes='Who can roll on the item', time='The time in seconds until the end of the bid')
-async def start_bids(interaction: Interaction, item: str, classes: str, time: Range[int, 1, 1000000]):
+async def start_random(interaction: Interaction, item: str, classes: str, time: Range[int, 1, 600]):
     """Starts a bid for an item."""
 
     view = RandomBidView(time)
@@ -201,23 +148,22 @@ async def start_bids(interaction: Interaction, item: str, classes: str, time: Ra
     await asyncio.sleep(time)
     view.stop()
 
-    random.shuffle(view.users)
-    winners_text = '\n'.join(user for user in view.users)
-    embed.description += f'\n\n**Results**:\n{winners_text or "No users joined!"}'
+    bid_entries = [f'{interaction.guild.get_member(user_id).display_name} ({bid_type})' for user_id, bid_type in view.user_bids.items() if interaction.guild.get_member(user_id)]
+    random.shuffle(bid_entries)
+    winners_text = '\n'.join(bid_entries)
+    embed.description += f'\n\n**Results**:\n{winners_text if winners_text else "No users joined!"}'
 
-    view.join.disabled = True
-    view.joinBox.disabled = True
-    view.joinAlt.disabled = True
-    view.leave.disabled = True
+    # Disable all buttons after the bidding period is over
+    for item in view.children:
+        item.disabled = True
 
     await interaction.edit_original_response(embed=embed, view=view)
 
 @bot.tree.command(name='bid')
 @app_commands.guild_only()
-@app_commands.default_permissions(administrator=True)
-@app_commands.describe(item='The name of the item', classes='Who can bid on the item', time='The time in seconds until the end of the bid')
+@app_commands.describe(item='The name of the item', classes='Who can bid on the item', time='The time in seconds until the end of the bid \n(There are 86400 seconds in a day)')
 async def start_bids(interaction: Interaction, item: str, classes: str, time: Range[int, 1, 600]):
-    view = PlatBidView(time, bot.min_bid)
+    view = PlatBidView(time)
     embed = discord.Embed(
         color=discord.Color.blue(),
         description=f'Now bidding: **{item}**!\n\nThe following may bid:\n**{classes}**'
@@ -229,8 +175,19 @@ async def start_bids(interaction: Interaction, item: str, classes: str, time: Ra
     view.stop()
 
     sorted_bids = view.get_sorted_bids()
+
+    # Determine the payment amount
+    if len(sorted_bids) >= 2:
+        payment_amount = sorted_bids[1][1] + 1  # Second place bid + 1
+    elif len(sorted_bids) == 1:
+        payment_amount = bot.min_bid + 1  # Minimum bid + 1 if only one bid
+    else:
+        payment_amount = None  # No bids were placed
+
     winners_text = '\n'.join(f'{user} bid {amount:,}' for user, amount in sorted_bids)
-    embed.description += f'\n\n**Results**:\n{winners_text or "No bids were placed!"}'
+    payment_text = f"Payment amount: {payment_amount:,}" if payment_amount is not None else "No bids were placed!"
+
+    embed.description += f'\n\n**Results**:\n{winners_text}\n\n{payment_text}'
 
     for item in view.children:
         item.disabled = True
@@ -239,10 +196,9 @@ async def start_bids(interaction: Interaction, item: str, classes: str, time: Ra
 
 @bot.tree.command(name='minbid')
 @app_commands.describe(min_bid='The minimum bid amount')
-@app_commands.default_permissions(administrator=True)
 async def set_min_bid(interaction: Interaction, min_bid: int):
     """Sets the minimum bid amount."""
-    bot.min_bid = max(1, min_bid)
-    await interaction.response.send_message(f"Minimum bid set to {bot.min_bid}.", ephemeral=True)
+    CustomBot.min_bid = max(1, min_bid)
+    await interaction.response.send_message(f"Minimum bid set to {CustomBot.min_bid}.", ephemeral=True)
 
 bot.run(config.TOKEN)
